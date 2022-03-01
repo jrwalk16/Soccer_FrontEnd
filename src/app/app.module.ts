117aa04af366faeb85081926df76f3be52dbb1ce
jeremy <jeremywalker1216@gmail.com>
2022-03-01 17:07:29 -0500

    added team component back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
 import { TeamComponent } from './team/team.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
-    TeamComponent
+    TeamComponent,
+    
   ],
   imports: [
     BrowserModule,
